c20a303cff07049d0c22d42e70b7241a3bef5aa3
Gionatan Danti <g.danti@assyoma.it>
2016-01-30 13:46:12 +0100

    Improved logcheck

--- logcheck.py
+++ logcheck.py
@@ -10,23 +10,28 @@ def parse_options():
     parser = optparse.OptionParser()
     parser.add_option("-l", "--logfile", dest="logfile", action="store",
                       help="Logfile", default="/var/log/psync/psync.log")
+    parser.add_option("-g", "--gap", dest="gap", action="store",
+                      help="Gap (in seconds)", default=900)
     return parser.parse_args()
 
 def scanlines():
     with open(options.logfile) as log:
         for line in log:
+            if 'f+++++++++' not in line and 'f.s.......' not in line:
+                continue
             match = re.search('\[(.*)\] \[.*\] \[.*\] \[.*\] \[.*\] ' +
                               '<f.[s+]....... (.*)', line)
             if match:
                 timestamp = time.mktime(time.strptime(match.group(1),
-                                   "%Y-%m-%d %H:%M:%S"))
-                L[match.group(2)] = timestamp
+                                                      "%Y-%m-%d %H:%M:%S"))
+                L[match.group(2)].append(timestamp)
                 continue
-            match = re.search('\[(.*)\] \[.*\] \[.*\] \[.*\] \[.*\] >f.[s+]....... (.*)', line)
+            match = re.search('\[(.*)\] \[.*\] \[.*\] \[.*\] \[.*\] ' +
+                              '>f.[s+]....... (.*)', line)
             if match:
                 timestamp = time.mktime(time.strptime(match.group(1),
-                                        "%Y-%m-%d %H:%M:%S"))
-                R[match.group(2)] = timestamp
+                                                      "%Y-%m-%d %H:%M:%S"))
+                R[match.group(2)].append(timestamp)
                 continue
 
 def compare():
@@ -35,7 +40,7 @@ def compare():
         if left_entry in R:
             ltime = L[left_entry]
             rtime = R[left_entry]
-            if abs(ltime-rtime) < 900:
+            if abs(ltime-rtime) < options.gap:
                 print "Double-edited entry: "+left_entry
             found = True
     return found
